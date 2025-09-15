041d7ea6812e2db925223417e544757005e829d9
Islam Albadawy <eslamalbadawy2000@gmail.com>
2025-09-15 04:16:29 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between">
-      <div className="font-bold">MachSchritt</div>
-      <div className="space-x-4">
-        <Link href="/">Startseite</Link>
-        <Link href="/ueber-uns">Über uns</Link>
-        <Link href="/fuer-einrichtungen">Für Einrichtungen</Link>
-        <Link href="/fuer-pflegekraefte">Für Pflegekräfte</Link>
-        <Link href="/fachbereiche">Fachbereiche</Link>
-        <Link href="/kontakt">Kontakt</Link>
-      </div>
+    <nav style={{ padding: "20px", background: "#0070f3", color: "#fff" }}>
+      <Link href="/">Startseite</Link> |{" "}
+      <Link href="/ueber-uns">Über uns</Link> |{" "}
+      <Link href="/einrichtungen">Für Einrichtungen</Link> |{" "}
+      <Link href="/pflegekraefte">Für Pflegekräfte</Link> |{" "}
+      <Link href="/fachbereiche">Fachbereiche</Link> |{" "}
+      <Link href="/kontakt">Kontakt</Link>
     </nav>
-  )
+  );
 }
