4fe3fdab87e9373d67df87545e68824e6638f979
Islam Albadawy <eslamalbadawy2000@gmail.com>
2025-09-15 04:35:42 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,19 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-16">
         <Link href="/">
-          <Image src="/logo.svg" alt="MachSchritt Logo" width={150} height={50} />
+          <Image src="/logo.svg" alt="Logo" width={120} height={40} />
         </Link>
         <div className="space-x-6">
-          <Link href="/ueber-uns" className="text-gray-700 hover:text-blue-600">
-            Über uns
-          </Link>
-          <Link href="/einrichtungen" className="text-gray-700 hover:text-blue-600">
-            Für Einrichtungen
-          </Link>
-          <Link href="/pflegekraefte" className="text-gray-700 hover:text-blue-600">
-            Für Pflegekräfte
-          </Link>
-          <Link href="/fachbereiche" className="text-gray-700 hover:text-blue-600">
-            Fachbereiche
-          </Link>
-          <Link href="/kontakt" className="text-gray-700 hover:text-blue-600">
-            Kontakt
-          </Link>
+          <Link href="/ueber-uns" className="hover:text-blue-600">Über uns</Link>
+          <Link href="/einrichtungen" className="hover:text-blue-600">Für Einrichtungen</Link>
+          <Link href="/pflegekraefte" className="hover:text-blue-600">Für Pflegekräfte</Link>
+          <Link href="/fachbereiche" className="hover:text-blue-600">Fachbereiche</Link>
+          <Link href="/kontakt" className="hover:text-blue-600">Kontakt</Link>
         </div>
       </div>
     </nav>
