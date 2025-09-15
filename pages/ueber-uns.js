8ac531cc361db645099db5243777a4fe27ea44a8
Islam Albadawy <eslamalbadawy2000@gmail.com>
2025-09-15 04:17:24 +0300

    Update ueber-uns.js

--- pages/ueber-uns.js
+++ pages/ueber-uns.js
@@ -1,13 +1,14 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 export default function UeberUns() {
   return (
-    <Layout title="Über uns">
-      <div className="p-10">
-        <h1 className="text-3xl font-bold mb-4">Über uns</h1>
-        <p>MachSchritt ist spezialisiert auf die Vermittlung qualifizierter Pflegekräfte für Einrichtungen in Deutschland.</p>
-        <img src="/images/nurse2.jpg" alt="Über uns" className="mt-6 rounded-lg shadow-lg w-1/2"/>
-      </div>
+    <Layout>
+      <h1>Über uns</h1>
+      <p>
+        MachSchritt ist ein Unternehmen, das sich auf die Vermittlung von
+        Pflegekräften spezialisiert hat. Wir unterstützen sowohl Einrichtungen
+        als auch Pflegekräfte auf ihrem Weg.
+      </p>
     </Layout>
-  )
+  );
 }
