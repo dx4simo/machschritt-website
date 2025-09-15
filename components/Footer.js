fe547c13e7bbf6ac14bd3a6d3479d8d1ab6eb06f
Islam Albadawy <eslamalbadawy2000@gmail.com>
2025-09-15 04:51:18 +0300

    Delete components directory

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6">
-      <p>© 2025 MachSchritt GmbH – Alle Rechte vorbehalten.</p>
-      <p>Pflegekräfte für Deutschland | Kontakt: info@machschritt.com</p>
-    </footer>
-  );
-}
