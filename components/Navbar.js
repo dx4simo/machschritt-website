6c271f28290c7862dd7e1e0952204112779534e6
Islam Albadawy <eslamalbadawy2000@gmail.com>
2025-09-15 04:19:03 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "20px", background: "#0070f3", color: "#fff" }}>
+    <nav style={{ padding: "15px", background: "#0070f3", color: "#fff" }}>
       <Link href="/">Startseite</Link> |{" "}
       <Link href="/ueber-uns">Über uns</Link> |{" "}
       <Link href="/einrichtungen">Für Einrichtungen</Link> |{" "}
